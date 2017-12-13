37dd82a109f1c4d6df75230cbbb5d8594cdfd7cf
Colin DeClue <cdeclue@rightpoint.com>
2017-12-13 11:45:38 -0600

    Day 13 works

--- Day13.cs
+++ Day13.cs
@@ -16,28 +16,10 @@ public static class Day13 {
 
     public static int Part2(string path) {
         var scanners = File.ReadLines(path)
-            .Select(l => l.Trim())
-            .Skip(1)
-            .Where(l => !string.IsNullOrEmpty(l))
-            .Select(l => l.Split(':').Select(p => int.Parse(p)).ToArray());
-        var delay = 156053;
-        var found = false;
-        var minFound = int.MaxValue;
-        var minFoundAt = int.MaxValue;
-        while(!found) {
-            var theseScanners = scanners
-                .Where(s => (s[0] + delay) % ((s[1] * 2) - 2) == 0);
-            var total = theseScanners.Count();
-            if(total < minFound) {
-                minFound = total;
-                minFoundAt = delay;                
-            }
-            if(total == 0) {
-                found = true;
-                break;
-            }
-            delay++;
-        }
+            .Select(l => l.Trim().Split(':').Select(p => int.Parse(p.Trim())).ToArray());
+            
+        var delay = 0;
+        while(scanners.Any(s => (s[0] + delay) % ((s[1] * 2) - 2) == 0)) delay++;
 
         return delay;
     }
