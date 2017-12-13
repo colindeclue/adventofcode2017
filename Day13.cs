53184c7a3212c932f27f664df50c5a634e96dcd3
Colin DeClue <cdeclue@rightpoint.com>
2017-12-13 11:49:07 -0600

    hurr durr don't parse everything every time

--- Day13.cs
+++ Day13.cs
@@ -16,8 +16,9 @@ public static class Day13 {
 
     public static int Part2(string path) {
         var scanners = File.ReadLines(path)
-            .Select(l => l.Trim().Split(':').Select(p => int.Parse(p.Trim())).ToArray());
-            
+            .Select(l => l.Trim().Split(':').Select(p => int.Parse(p.Trim())).ToArray())
+            .ToArray();
+        
         var delay = 0;
         while(scanners.Any(s => (s[0] + delay) % ((s[1] * 2) - 2) == 0)) delay++;
 
